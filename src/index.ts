dcd0b8e721d66f6c81cea3c3da4e3ba80b3ca924
Krishna Basak <>
2026-01-04 15:36:54 +0530

    feat: use top-level await in the application

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-// oxlint-disable prefer-await-to-then
 import { env } from '@/configs/environment';
 import dataSource from '@/database/source';
 import { server } from '@/server';
-import logger from '@/utils/logger';
 
-dataSource
-  .initialize()
-  .then(() => {
-    logger.info('started the server on the designated port');
-  })
-  .catch((error) => {
-    logger.error(
-      'failed to start the server due to error establishing database connection',
-      {
-        error,
-      },
-    );
-  });
+await dataSource.initialize();
 
 const app = { port: env.SERVER_PORT, fetch: server.fetch };
 export default app;
